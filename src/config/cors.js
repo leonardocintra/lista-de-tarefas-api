6be55c9ca0918abb4092eb5779e03cdc664579da
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-05-01 14:39:25 -0300

    mudando a porta para heroku

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,6 +1,9 @@
 module.exports = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET, POS, OPTION, PUT, PATH, DELETE')
-    res.header('Access-Control-Allow-headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    // Aqui é todos os enderecos que podem chamar.
+    res.header('Access-Control-Allow-Origin', '*'),
+    // Quais verbos a aplicação vai permitir
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    // Tipos de headers
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next()
 }
